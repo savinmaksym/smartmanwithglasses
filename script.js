613085d6870be6207f9c3bde00eae107d9f571d9
chill-yadro <Savi.xd7s@gmail.com>
2025-07-09 19:45:10 +0300

    added random fact

--- script.js
+++ script.js
@@ -3,5 +3,18 @@
 function showImage() {
     document.getElementById('showImageBtn').style.display = 'none';
     document.getElementById('myImage').style.display = 'block';
-    document.getElementById('music').play()
+    document.getElementById('music').play();
+}
+
+function showRandomFact() {
+    fetch("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("fact_text").textContent = data.text;
+            document.getElementById('music').play();
+            document.getElementById('showImageBtn').style.display = 'none';
+            document.getElementById('myImage').style.display = 'none';
+            document.getElementById('fact_text').style.display = 'block';
+        });
+    
 }
